7f56cfc2e8f53e19b35738e7c74e19740323a074
wzc11 <569376037@qq.com>
2013-12-10 00:30:16 +0800

    增加扒取下载内容
    增加扒取下载内容

--- src/DataServer/grubber/test.py
+++ src/DataServer/grubber/test.py
@@ -7,9 +7,10 @@ import debuger
 from hunter_learn import hunter_learn
 
 
-print sys.argv[1], sys.argv[2]
+#print sys.argv[1], sys.argv[2]
 h = hunter_learn(sys.argv[1], sys.argv[2])
-l = h.getInfo()
-#debuger.printer(l)
-for course in l:
-    debuger.printer(h.getSpecial(course['id']))
+l=h.getHomework("http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/hom_wk_detail.jsp?id=538195&course_id=103507&rec_id=null")
+#l = h.getInfo()
+debuger.printer(l)
+#for course in l:
+#    debuger.printer(h.getSpecial(course['id']))
