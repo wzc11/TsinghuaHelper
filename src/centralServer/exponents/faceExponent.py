1cec0bf27304f792a3345ba9488d3ad803b54946
hnkfyjb <yanjb11@mails.tsinghua.edu.cn>
2013-12-16 14:41:35 +0800

    实现屌丝指数和学霸指数

---
+++ src/centralServer/exponents/faceExponent.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'YJB'
+
+from PIL import Image
+
+def faceExponent(src):
+    im = Image.open(src.decode('UTF-8'))
+    new_image = im.convert('L')
+    sequence = new_image.getdata()
+    data_num = 0.0
+    average = 0.0
+    for i in sequence:
+        data_num = data_num + 1
+        average += sequence[i]
+    average /= data_num
+    average -= 140
+    average /= 2
+    print(average)
+    return average
+
